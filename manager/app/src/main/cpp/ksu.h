241a847817eae600ef5e95e13499ec51f8c006eb
weishu <twsxtd@gmail.com>
2025-11-03 10:23:34 +0800

    manager: fix legacy get version

--- manager/app/src/main/cpp/ksu.h
+++ manager/app/src/main/cpp/ksu.h
@@ -7,6 +7,7 @@
 
 #include <stdint.h>
 #include <sys/ioctl.h>
+#include <utility>
 
 uint32_t get_version();
 
@@ -175,8 +176,14 @@ bool is_kernel_umount_enabled();
 #define KSU_IOCTL_GET_FEATURE _IOC(_IOC_READ|_IOC_WRITE, 'K', 13, 0)
 #define KSU_IOCTL_SET_FEATURE _IOC(_IOC_WRITE, 'K', 14, 0)
 
-bool get_allow_list(struct ksu_get_allow_list_cmd*);
+bool get_allow_list(struct ksu_get_allow_list_cmd *);
 
-int legacy_get_version(void);
+inline std::pair<int, int> legacy_get_info() {
+    int32_t version = -1;
+    int32_t flags = 0;
+    int32_t result = 0;
+    prctl(0xDEADBEEF, 2, &version, &flags, &result);
+    return {version, flags};
+}
 
 #endif //KERNELSU_KSU_H
